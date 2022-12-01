7fae30a373fa43a9c2365f9b6672a5daa5501f26
saumyanayak <saumyanayak264@gmail.com>
2022-12-01 22:15:42 +0530

    fixed some changes

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,8 +1,4 @@
 module.exports = {
   root: true,
-  extends: ['weblint'],
-  //dont's push this file
-  rules: {
-    'prettier/prettier': ['error', { endOfLine: 'auto' }]
-  }
+  extends: ['weblint']
 };
